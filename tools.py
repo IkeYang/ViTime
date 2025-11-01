acff20862bb50760c7f4565be564a516e8026dd9
Luoxiao Yang <42018430+IkeYang@users.noreply.github.com>
2025-11-01 20:37:35 +0800

    Add temperature and sampleNumber parameters to inference

--- tools.py
+++ tools.py
@@ -24,17 +24,20 @@ class ViTimePredictor:
         self,
         device: str = 'cuda:0',
         model_name: str = 'MAE',
+        tempature=1,
 
     ) -> None:
         
         model_path_env = config.VITIME_MODEL_PATH  
-    
+        self.tempature=tempature
         self._iface = InferenceInterface(model_path_env,  model_name=model_name, device=device)
 
-    def __call__(self, time_series, future_length) -> np.ndarray:
+    def __call__(self, time_series, future_length,sampleNumber) -> np.ndarray:
       
         pred = self._iface.inference(
             np.asarray(time_series),
-            future_length
+            future_length,
+            sampleNumber,
+            tempature=self.tempature
         )
-        return pred.flatten()
+        return pred
